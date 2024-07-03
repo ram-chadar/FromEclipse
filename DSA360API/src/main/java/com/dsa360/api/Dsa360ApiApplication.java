bd7c4df9cc0f7dd7cae8ff3da3f933d84322f824
ram-chadar <javaprojectwork2715@gmail.com>
2024-07-03 16:10:10 +0530

    added started

--- DSA360API/src/main/java/com/dsa360/api/Dsa360ApiApplication.java
+++ DSA360API/src/main/java/com/dsa360/api/Dsa360ApiApplication.java
@@ -8,6 +8,7 @@ public class Dsa360ApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Dsa360ApiApplication.class, args);
+		System.out.println("started");
 	}
 
 }
